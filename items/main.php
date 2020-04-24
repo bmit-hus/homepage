fd664ff2c5d837facc7b0e296fb1069d6be4b357
bmit-hus <61005372+bmit-hus@users.noreply.github.com>
2020-04-24 20:43:26 +0200

    small changesm nothing important.

--- items/main.php
+++ items/main.php
@@ -11,22 +11,21 @@ $open_content = '<section class="content element">';
 $load_content = $parsedown->text(file_get_contents("page/$page.md"));
 $shut_content = '</section>';
 
-
 echo $open_main;
 
-    if ( $page != "blog" )
-    {
-        include "items/navbar.php";
-    }
-    
-    // sidebar
-    echo $open_aside;
-    echo $load_aside;
-    echo $shut_aside;
-    
-    // page content
-    echo $open_content;
-    echo $load_content;
-    echo $shut_content;
+if ( $page != "blog" )
+{
+    include "items/navbar.php";
+}
+
+// sidebar
+echo $open_aside;
+echo $load_aside;
+echo $shut_aside;
+
+// page content
+echo $open_content;
+echo $load_content;
+echo $shut_content;
 
 echo $shut_main;
