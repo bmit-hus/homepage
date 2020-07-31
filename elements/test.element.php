885a08baacdbcd0f4a458f32787894202b14f6ff
bmit-hus <61005372+bmit-hus@users.noreply.github.com>
2020-07-31 21:30:52 +0200

    exported test element file

---
+++ elements/test.element.php
@@ -0,0 +1,20 @@
+<?php
+
+echo <<<TEST
+
+<h1>chapter</h1>
+<p>This is a test paragraph.</p>
+<h2>subchapter</h2>
+<p>This is a test paragraph.</p>
+<h3>subsubchapter</h3>
+<p>This is a test paragraph.</p>
+<h4>subsubsubchapter</h4>
+<p>This is a test paragraph.</p>
+<h5>subsubsubsubchapter</h5>
+<p>This is a test paragraph.</p>
+<h6>subsubsubsubsubchapter</h6>
+<p>This is a test paragraph.</p>
+
+TEST;
+
+// EOF
--- index.php
+++ index.php
@@ -3,4 +3,6 @@
 $goto_root = '.';
 include_once "$goto_root/includes/global.inc.php";
 
+include_once "$goto_root/elements/test.element.php";
+
 // EOF
