e23781d648d904fefb0f2342b932d3503bcb7be2
bmit-hus <61005372+bmit-hus@users.noreply.github.com>
2020-08-09 22:26:13 +0200

    made nav functional.

--- assets/elements/navigation.php
+++ assets/elements/navigation.php
@@ -3,11 +3,10 @@
 echo <<<NAV
 
 <ul class="nav-menu">
-  <li class="nav-menu-item home"> home</li>
-
-  <li class="nav-menu-item about"> about</li>
-  <li class="nav-menu-item blog"> blog</li>
-  <li class="nav-menu-item contact"> contact</li>
+  <li class="nav-menu-item home">     <a href="$root/index.php"         class="nav-menu-link home">     home</a></li>
+  <li class="nav-menu-item about">    <a href="$root/pages/about.php"   class="nav-menu-link about">    about</a></li>
+  <li class="nav-menu-item blog">     <a href="$root/pages/blog.php"    class="nav-menu-link blog">     blog</a></li>
+  <li class="nav-menu-item contact">  <a href="$root/pages/contact.php" class="nav-menu-link contact">  contact</a></li>
 </ul>
 
 NAV;
