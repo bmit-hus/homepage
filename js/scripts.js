a3dd067011484e61a4cd7dfd085aaa30beffdc9a
bmit-hus <61005372+bmit-hus@users.noreply.github.com>
2020-08-17 19:09:45 +0200

    clean up crew!

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 
 // PAGE MENU manipulation
-
 $('.page-menu--icon').click( function() {
 
   $('.page-menu--container').toggleClass('page-menu--display');
@@ -8,18 +7,3 @@ $('.page-menu--icon').click( function() {
 
 })
 
-
-
-  // $('.hamburger--button').click(function() {
-
-  //   // hide header elements
-  //   $('.home-button').toggleClass('hidden-on-mobile');
-  //   $('.page-name').toggleClass('hidden-on-mobile');
-
-  //   // display nav menu
-  //   $('.page-strip--element').toggleClass('.page-strip--open-element');
-
-  //   // transform burger menu to X
-  //   $(this).toggleClass('burger-menu--open');
-
-  // })
