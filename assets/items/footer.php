5227a4bb706be02f01e1646c7ca4c85b94a68794
bmit-hus <61005372+bmit-hus@users.noreply.github.com>
2020-09-04 08:30:42 +0200

    minor changes.

--- assets/items/footer.php
+++ assets/items/footer.php
@@ -5,11 +5,27 @@ echo <<<FOOTER
 
   <footer>
 
-  <p>$copyright $author, $date__yyyy. </p>
+    <p>
+      $copyright $author, $date__yyyy.
+    </p>
 
-  <p>Domain is sponsered by <a href="https://www.espas.ch/">ESPAS</a>. </p>
-<p></p>
+    <p>
+      This domain is sponsered by 
+        <a href="https://www.espas.ch/"> ESPAS</a>
+      . 
 
+      Check out 
+        <a href=""> this site</a>
+      to see their services or follow  
+        <a href=""> this link</a>
+      to inform yourself about their IT services by their subsidiary
+      IT services company. The name of this company is 
+        <em> IT Services by ESPAS</em>
+      .
+      I am also working for this subsidiary company.
+    </p> 
+
+    <p></p>
 
   </footer>
 
