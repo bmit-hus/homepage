1c1b3ecd96d68216fdda6ceead965d274b89f24b
bmit-hus <61005372+bmit-hus@users.noreply.github.com>
2020-09-09 16:36:20 +0200

    something something special.

--- assets/items/footer.php
+++ assets/items/footer.php
@@ -5,31 +5,39 @@ $espas_homepage =     "https://www.espas.ch/";
 $espas_services =     "https://www.espas.ch/de/unsere-dienstleistungen";
 $espas_it_services =  "https://its-espas.ch/";
 
-echo <<<FOOTER
+?>
 
-  <footer>
+<footer>
 
-    <p>
-      <strong>
-        $copyright $author, $date__yyyy.
-      </strong>
-    </p>
+<p>
+  <strong> 
+    <?= "$copyright $author, $date__yyyy." ?>
+  </strong>
+</p>
 
-    <p>
-      This domain is sponsered by 
-        <a href="$espas_homepage" target="_blank"> 
-          ESPAS</a>. 
+<p>
+  This domain is sponsered by 
+    <a 
+      href=
+        <?=$espas_homepage?>
+      target="_blank"> 
+      ESPAS
+    </a>. 
 
-      Check out our
-        <a href="$espas_services" target="_blank"> 
-          offer of services</a>, 
-      especially our
-        <a href="$espas_it_services" target="_blank"> 
-          IT services</a>.
-    </p>
+  Check out our
+    <a 
+      href=
+        <?=$espas_services?>
+      target="_blank"> 
+      offer of services
+    </a>, 
+  especially our
+    <a 
+      href=
+        <?=$espas_it_services?> 
+      target="_blank"> 
+      IT services
+    </a>.
+</p>
 
-  </footer>
-
-FOOTER;
-
-// EOF
+</footer>
