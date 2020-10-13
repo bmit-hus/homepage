92478a3c421524fa42175ce0e9bacaf18c9b0616
bmit-hus <61005372+bmit-hus@users.noreply.github.com>
2020-10-13 16:31:59 +0200

    reintroduced everthing EXEPT VARIABLES FROM FOOTER

--- assets/arch.php
+++ assets/arch.php
@@ -3,6 +3,7 @@
 include_once "classes/meta.class.php"; $meta = new Meta;
 include_once "$ROOT/classes/parsedown.class.php";
 include_once "$ROOT/assets/functions.php";
+include_once "$ROOT/assets/variables.php";
 
 $parsedown = new Parsedown;
 
