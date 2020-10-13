60a82c1c469a3a5edc8fd1799c2f61a4e782192a
bmit-hus <61005372+bmit-hus@users.noreply.github.com>
2020-10-13 16:27:37 +0200

    copied footer.

--- index.php
+++ index.php
@@ -7,5 +7,40 @@ include_once "assets/arch.php";
 ?>
 
 <header>header</header>
+
 <main> <?php getFillerContent(); ?> </main>
-<footer>footer</footer>
+
+<footer>
+
+<p>
+  <strong> 
+    <?= "$copyright $author, $date__yyyy." ?>
+  </strong>
+</p>
+
+<p>
+  This domain is sponsered by 
+    <a 
+      href=
+        <?=$espas_homepage?>
+      target="_blank"> 
+      ESPAS
+    </a>. 
+
+  Check out our
+    <a 
+      href=
+        <?=$espas_services?>
+      target="_blank"> 
+      offer of services
+    </a>, 
+  especially our
+    <a 
+      href=
+        <?=$espas_it_services?> 
+      target="_blank"> 
+      IT services
+    </a>.
+</p>
+
+</footer>
